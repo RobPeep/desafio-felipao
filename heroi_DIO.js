bcb453e065e55efa6ac38c5a5e2a189ca360cb56
RobPeep <raulrobert73@gmail.com>
2025-10-15 00:04:17 -0300

    desafio do heroi com o código corrigido e pronto

--- heroi_DIO.js
+++ heroi_DIO.js
@@ -1,26 +1,27 @@
-let nomeHeroi = "RobPeep"
+let nomeHeroi = "RobPeep";
+let XP = 10999;
 
-let XP = 3000
-
-if(XP < 1000){
-    console.log("O Herói " + nomeHeroi + ", está no nivel de Ferro")
+if (XP < 1000) {
+  console.log("O Herói " + nomeHeroi + " está no nível de Ferro");
+} 
+else if (XP >= 1000 && XP < 2000) {
+  console.log("O Herói " + nomeHeroi + " está no nível de Bronze");
+} 
+else if (XP >= 2000 && XP < 5000) {
+  console.log("O Herói " + nomeHeroi + " está no nível de Prata");
+} 
+else if (XP >= 5000 && XP < 7000) {
+  console.log("O Herói " + nomeHeroi + " está no nível de Ouro");
+} 
+else if (XP >= 7000 && XP < 8000) {
+  console.log("O Herói " + nomeHeroi + " está no nível de Platina");
+} 
+else if (XP >= 8000 && XP < 9000) {
+  console.log("O Herói " + nomeHeroi + " está no nível de Ascendente");
+} 
+else if (XP >= 9000 && XP < 10000) {
+  console.log("O Herói " + nomeHeroi + " está no nível de Imortal");
+} 
+else {
+  console.log("O Herói " + nomeHeroi + " está no nível de Radiante");
 }
-else if(XP > 1001 / XP < 2000){
-    console.log("O Herói " + nomeHeroi + ", está no nivel de Bronze")
-}
-else if(XP > 2001/ XP < 5000){
-    console.log("O Herói " + nomeHeroi + ", está no nivel de Prata")
-}
-else if(XP > 5001/ XP < 7000){
-    console.log("O Herói " + nomeHeroi + ", está no nivel de Ouro")
-}
-else if(XP > 7001 / XP < 8000){
-    console.log("O Herói " + nomeHeroi + ", está no nivel de Platina")
-}
-else if(XP > 8001 / XP < 9000){
-    console.log("O Herói " + nomeHeroi + ", está no nivel de Ascendente")
-}
-else if(XP > 9001 / XP < 10000){
-    console.log("O Herói " + nomeHeroi + ", está no nivel de Imortal")
-}
-else {console.log("O Herói " + nomeHeroi + ", está no nivel de Radiante")}
